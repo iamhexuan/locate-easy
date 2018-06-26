b5424eb1ab3ce13815a18ed214e09ae07b09e0f3
he xuan <iamhexuan@gmail.com>
2018-06-27 00:49:14 +0800

    style path

--- js/angular/elMapDirective/elMap.js
+++ js/angular/elMapDirective/elMap.js
@@ -25,7 +25,8 @@ app.directive("elMap", function() {
 			function drawPath(path) {
 				line.remove();
 				line = new Path();
-				line.strokeColor = "red";
+				line.strokeColor = "#3b8ac8";
+				line.strokeWidth = 8;
 
 				var i, position;
 				for (i = 0; i < path.length; i++) {
@@ -34,6 +35,43 @@ app.directive("elMap", function() {
 						new Point(position.position._x, position.position._y)
 					);
 				}
+
+				roundCorners(line, 10);
+			}
+
+			function roundCorners(path, radius) {
+				var segments = path.segments.slice(0);
+				path.removeSegments();
+
+				for (var i = 0, l = segments.length; i < l; i++) {
+					var curPoint = segments[i].point;
+					var nextPoint = segments[i + 1 == l ? 0 : i + 1].point;
+					var prevPoint =
+						segments[i - 1 < 0 ? segments.length - 1 : i - 1].point;
+					var nextDelta = curPoint.subtract(nextPoint);
+					var prevDelta = curPoint.subtract(prevPoint);
+
+					nextDelta.length = radius;
+					prevDelta.length = radius;
+
+					path.add(
+						new paper.Segment(
+							curPoint.subtract(prevDelta),
+							null,
+							prevDelta.divide(2)
+						)
+					);
+
+					path.add(
+						new paper.Segment(
+							curPoint.subtract(nextDelta),
+							nextDelta.divide(2),
+							null
+						)
+					);
+				}
+				path.closed = false;
+				return path;
 			}
 		}
 	};
